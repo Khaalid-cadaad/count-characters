4b6981caa8a8721be6332719f06e14309687bfe4
unknown <khalidabdi3101@gmail.com>
2024-10-03 05:38:48 +0300

    Done

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-// Good Luck 💪🏾
+
+function count(character, str) {
+    
+    if (character.length !== 1) {
+        throw new Error("First argument must be a single character.");
+    }
+    
+    
+    return str.split(character).length - 1;
+}
+
+console.log(count("a","fikircap"));
+console.log(count("c","bootcamp"));
+console.log(count("o","fikircamp coding bootcamp"));
